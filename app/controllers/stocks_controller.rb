12ae94463f6df418e25545653e45ebd6b723fd19
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-02-13 09:36:45 +0300

    add results partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else 
+        flash[:alert] = "ТАКОГО ТИКЕРА НЕ СУЩЕСТВУЕТ"
+        redirect_to my_portfolio_path
+      end
+    else 
+      flash[:alert] = "Введите символ тикера !!!!!!!!!!!!!!!!!!!!!"
+      redirect_to my_portfolio_path
+    end
   end
 
 end
